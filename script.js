cca3f2c6d250aab0ad123d64b937b8f43fa9bb50
Sophie Cheng <chengsophie25@gmail.com>
2022-06-24 15:49:39 +0200

    charts

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 // Create element canvas *****
-// var createDiv = document.createElement("div");
-// var createCanvas = document.createElement("canvas");
-// createCanvas.setAttribute("id", "myChart");
-// var getTable = document.getElementById("table1");
-// createDiv.appendChild(createCanvas);
-// getTable.insertBefore(createDiv, getTable.children[0]);
 var getDiv = document.getElementById("Crimes_et_d.C3.A9lits_enregistr.C3.A9s_par_les_services_de_police");
 var createDiv = document.createElement("div");
 var createCanvas = document.createElement("canvas");
 createCanvas.setAttribute("id", "myChart");
 createDiv.appendChild(createCanvas);
 getDiv.appendChild(createDiv);
+createCanvas.style.width = '1000px';
+createCanvas.style.height = '1000px';
 
 // create element canvas in table 2
 var getTable2 = document.querySelector('#Homicides');
@@ -20,6 +16,7 @@ createCanvas2.setAttribute('id', 'myChart2');
 createDiv2.appendChild(createCanvas2);
 getTable2.appendChild(createDiv2);
 // *****
+
 
 // Colors bars ***** 
 // var barColors = [
@@ -44,50 +41,38 @@ var barColor1 = [
 var barColor2 = [
   'rgba(75, 192, 192, 0.2)',
 ]
+var barColor3 = [
+  'rgba(255, 159, 64, 0.2)',
+]
+var barColor4 = [
+  'rgba(54, 162, 235, 0.2)',
+]
+var barColor5 = [
+  'rgba(255, 206, 86, 0.2)',
+]
+var barColor6 = [
+  'rgba(153, 102, 255, 0.2)',
+]
+
 var borderColor1 = [
   'rgba(255, 99, 132, 1)',
 ]
 var borderColor2 = [
   'rgba(75, 192, 192, 1)',
 ]
-var barColor3 = [
-  'rgba(255, 159, 64, 0.2)',
-]
 var borderColor3 = [
   'rgba(255, 159, 64, 1)'
 ]
+var borderColor4 = [
+  'rgba(54, 162, 235, 1)',
+]
+var borderColor5 = [
+  'rgba(255, 206, 86, 1)',
+]
+var borderColor6 = [
+  'rgba(153, 102, 255, 1)',
+]
 // *****
-
-// test get data 
-// var getTable = document.getElementById("table1");
-// Get data table 1
-// var rowLength = getTable.rows.length;
-// for(i = 1; i < rowLength; i++){
-//   var getCells = getTable.rows.item(i).cells;
-//   var cellLength = getCells.length; 
-//   for(var j = 0; j < cellLength; j++){
-//     var cellValue = getCells.item(j).innerHTML;
-//     console.log(cellValue[1]);
-//   }
-// }
-
-// var yearData = document.querySelectorAll("#table1 tr");
-// for(let i = 0; i < yearData.length; i++){
-//   yearData[1].style.backgroundColor = 'green';
-//   let yearsTable1 = yearData[1].innerText;
-//   console.log(yearsTable1);
-// }
-
-// var tableData = document.querySelectorAll("#table1 td");
-// for(let i=0; i<tableData.length; i++){
-//   console.log(tableData[i]);
-// }
-// tableData[0].style.backgroundColor = 'green';
-
-// array table 1 in console
-// const selectTable1 = document.querySelector('#table1');
-// const arr1 = [...selectTable1.rows].map(r => [...r.querySelectorAll('td')].map(td => td.textContent));
-// console.log(arr1);
 
 // data table 1
 let table1 = document.querySelector('#table1');
@@ -103,201 +88,85 @@ let date8Table1 = [];
 let date9Table1 = [];
 let date10Table1 = [];
 let date11Table1 = [];
-// let dates = [];
 let dates = [2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012];
+
+function replaceNumDot(decimal) {
+    return parseFloat((decimal + "").replace(",", "."));
+}
 
 for(let i = 2; i < table1.rows.length; i++){
   let row1 = table1.rows[i];
   let countries1 = row1.cells[1].innerText;
-  // console.log(countries1);
+//   console.log(countries1);
   countryTable1.push(countries1);
 }
 for(let i = 2; i < table1.rows.length; i++){
   let row1 = table1.rows[i];
   let date1in1 = row1.cells[2].innerText;
-  console.log(date1in1);
-  date1Table1.push(date1in1);
+//   console.log(replaceNumDot(date1in1));
+  date1Table1.push(replaceNumDot(date1in1));
 }
 for(let i = 2; i < table1.rows.length; i++){
   let row1 = table1.rows[i];
   let date2in1 = row1.cells[3].innerText;
-  // console.log(date2in1);
-  date2Table1.push(date2in1);
+//   console.log(replaceNumDot(date2in1));
+  date2Table1.push(replaceNumDot(date2in1));
 }
 for(let i = 2; i < table1.rows.length; i++){
   let row1 = table1.rows[i];
   let date3in1 = row1.cells[4].innerText;
-  // console.log(date3in1);
-  date3Table1.push(date3in1);
+//   console.log(replaceNumDot(date3in1));
+  date3Table1.push(replaceNumDot(date3in1));
 }
 for(let i = 2; i < table1.rows.length; i++){
   let row1 = table1.rows[i];
   let date4in1 = row1.cells[5].innerText;
-  // console.log(date4in1);
-  date4Table1.push(date4in1);
+//   console.log(replaceNumDot(date4in1));
+  date4Table1.push(replaceNumDot(date4in1));
 }
 for(let i = 2; i < table1.rows.length; i++){
   let row1 = table1.rows[i];
   let date5in1 = row1.cells[6].innerText;
-  // console.log(date5in1);
-  date5Table1.push(date5in1);
+//   console.log(replaceNumDot(date5in1));
+  date5Table1.push(replaceNumDot(date5in1));
 }
 for(let i = 2; i < table1.rows.length; i++){
   let row1 = table1.rows[i];
   let date6in1 = row1.cells[7].innerText;
-  // console.log(date6in1);
-  date6Table1.push(date6in1);
+//   console.log(replaceNumDot(date6in1));
+  date6Table1.push(replaceNumDot(date6in1));
 }
 for(let i = 2; i < table1.rows.length; i++){
   let row1 = table1.rows[i];
   let date7in1 = row1.cells[8].innerText;
-  // console.log(date7in1);
-  date7Table1.push(date7in1);
+//   console.log(replaceNumDot(date7in1));
+  date7Table1.push(replaceNumDot(date7in1));
 }
 for(let i = 2; i < table1.rows.length; i++){
   let row1 = table1.rows[i];
   let date8in1 = row1.cells[9].innerText;
-  // console.log(date8in1);
-  date8Table1.push(date8in1);
+//   console.log(replaceNumDot(date8in1));
+  date8Table1.push(replaceNumDot(date8in1));
 }
 for(let i = 2; i < table1.rows.length; i++){
   let row1 = table1.rows[i];
   let date9in1 = row1.cells[10].innerText;
-  // console.log(date9in1);
-  date9Table1.push(date9in1);
+//   console.log(replaceNumDot(date9in1));
+  date9Table1.push(replaceNumDot(date9in1));
 }
 for(let i = 2; i < table1.rows.length; i++){
   let row1 = table1.rows[i];
   let date10in1 = row1.cells[11].innerText;
-  // console.log(date10in1);
-  date10Table1.push(date10in1);
+//   console.log(replaceNumDot(date10in1));
+  date10Table1.push(replaceNumDot(date10in1));
 }
 for(let i = 2; i < table1.rows.length; i++){
   let row1 = table1.rows[i];
   let date11in1 = row1.cells[12].innerText;
-  // console.log(date11in1);
-  date11Table1.push(date11in1);
+//   console.log(replaceNumDot(date11in1));
+  date11Table1.push(replaceNumDot(date11in1));
 }
 
-// Create chart js 1
-// var getChart1 = document.getElementById("myChart");
-
-// var dataFirst = {
-//   label: '2002',
-//   data: date1Table1,
-//   backgroundColor: barColor1,
-//   borderColor: borderColor1,
-//   borderWidth: 1,
-//   yAxisID: "y-axis-density"
-// };
-// var dataSecond = {
-//   label: '2003',
-//   data: date2Table1,
-//   backgroundColor: barColor2,
-//   borderColor: borderColor2,
-//   borderWidth: 1,
-//   yAxisID: "y-axis-gravity"
-// };
-// var dataThird = {
-//   label: '2004',
-//   data: date3Table1,
-//   backgroundColor: barColor1,
-//   borderColor: borderColor1,
-//   borderWidth: 1,
-//   yAxisID: "y-axis-gravity"
-// };
-// var dataFourth = {
-//   label: '2005',
-//   data: date4Table1,
-//   backgroundColor: barColor2,
-//   borderColor: borderColor2,
-//   borderWidth: 1,
-//   yAxisID: "y-axis-gravity"
-// };
-// var dataFifth = {
-//   label: '2006',
-//   data: date5Table1,
-//   backgroundColor: barColor1,
-//   borderColor: borderColor1,
-//   borderWidth: 1,
-//   yAxisID: "y-axis-gravity"
-// };
-// var dataSixth = {
-//   label: '2007',
-//   data: date6Table1,
-//   backgroundColor: barColor2,
-//   borderColor: borderColor2,
-//   borderWidth: 1,
-//   yAxisID: "y-axis-gravity"
-// };
-// var dataSeventh = {
-//   label: '2008',
-//   data: date7Table1,
-//   backgroundColor: barColor1,
-//   borderColor: borderColor1,
-//   borderWidth: 1,
-//   yAxisID: "y-axis-gravity"
-// };
-// var dataEighth = {
-//   label: '2009',
-//   data: date8Table1,
-//   backgroundColor: barColor2,
-//   borderColor: borderColor2,
-//   borderWidth: 1,
-//   yAxisID: "y-axis-gravity"
-// };
-// var dataNinth = {
-//   label: '2010',
-//   data: date9Table1,
-//   backgroundColor: barColor1,
-//   borderColor: borderColor1,
-//   borderWidth: 1,
-//   yAxisID: "y-axis-gravity"
-// };
-// var dataTenth = {
-//   label: '2011',
-//   data: date10Table1,
-//   backgroundColor: barColor2,
-//   borderColor: borderColor2,
-//   borderWidth: 1,
-//   yAxisID: "y-axis-gravity"
-// };
-// var dataEleventh = {
-//   label: '2012',
-//   data: date11Table1,
-//   backgroundColor: barColor1,
-//   borderColor: borderColor1,
-//   borderWidth: 1,
-//   yAxisID: "y-axis-gravity"
-// };
- 
-// var dataCountry1 = {
-//   labels: countryTable1,
-//   datasets: [dataFirst, dataSecond, dataThird, dataFourth, dataFifth, dataSixth, dataSeventh, dataEighth, dataNinth, dataTenth, dataEleventh]
-// };
- 
-// var chartOptions1 = {
-//   scales: {
-//     xAxes: [{
-//       barPercentage: 1,
-//       categoryPercentage: 0.6
-//     }],
-//     yAxes: [{
-//       id: "y-axis-density"
-//     }, {
-//       id: "y-axis-gravity"
-//     }]
-//   }
-// };
-// var barChart1 = new Chart(getChart1, {
-//   type: 'line',
-//   data: dataCountry1,
-//   options: chartOptions1
-// });
-// ***** 
-
-
-// test
 var getChart1 = document.getElementById("myChart");
 var xValues = countryTable1;
 
@@ -306,7 +175,7 @@ new Chart(getChart1, {
   data: {
     labels: xValues,
     datasets: [{ 
-      data: dates,
+      data: date1Table1,
       backgroundColor: barColor1,
       borderColor: borderColor1,
       fill: false,
@@ -318,48 +187,78 @@ new Chart(getChart1, {
       borderColor: borderColor2,
       fill: false,
       label: "2003",
-    }, { 
+    },
+    {
       data: date3Table1,
       backgroundColor: barColor3,
       borderColor: borderColor3,
       fill: false,
       label: "2004",
-    }, {
-      data: date4Table1,
-      backgroundColor: barColor1,
-      borderColor: borderColor1,
+    },
+    { data: date4Table1, 
+      backgroundColor: barColor4,
+      borderColor: borderColor4,
       fill: false,
       label: "2005",
-    }
+    },
+    {
+      data: date5Table1,
+      backgroundColor: barColor5,
+      borderColor: borderColor5,
+      fill: false,
+      label: "2006",
+    },
+    {
+      data: date6Table1,
+      backgroundColor: barColor6, 
+      borderColor: borderColor6,
+      fill: false,
+      label: "2007",
+    },
+    {
+      data: date7Table1,
+      backgroundColor: barColor1, 
+      borderColor: borderColor1,
+      fill: false,
+      label: "2008",
+    },
+    {
+      data: date8Table1,
+      backgroundColor: barColor2, 
+      borderColor: borderColor2,
+      fill: false,
+      label: "2009",
+    },
+    {
+      data: date9Table1,
+      backgroundColor: barColor3, 
+      borderColor: borderColor3,
+      fill: false,
+      label: "2010",
+    },
+    {
+      data: date10Table1,
+      backgroundColor: barColor4, 
+      borderColor: borderColor4,
+      fill: false,
+      label: "2011",
+    },
+    {
+      data: date11Table1,
+      backgroundColor: barColor5, 
+      borderColor: borderColor5,
+      fill: false,
+      label: "2012",
+    },
   ]
   },
   options: {
     legend: {display: true}
   }
 });
-
-
+// ***** 
 
 // Graph table 2 ****************************************************
-
-// const tbl = document.querySelector("#table2");
-// for(let row of tbl.rows) {
-//   console.log(row.cells[1].innerHTML);
-// }
-// const table2 = document.querySelector("#table2");
-// for(let row of table2.rows) {
-//   console.log(row.cells[2].innerHTML);
-// }
-// const tablerow3 = document.querySelector("#table2");
-// for(let row of tablerow3.rows) {
-//   console.log(row.cells[3].innerHTML);
-// }
-
-// array in console 
-// const selectTable2 = document.querySelector('#table2')
-// const arr = [...selectTable2.rows].map(r => [...r.querySelectorAll('td')].map(td => td.textContent));
-// console.log(arr);
-
 let table2 = document.getElementById('table2');
 let countryTable2 = [];
 let date1Table2 = [];
@@ -412,9 +311,9 @@ var chartOptions = {
       categoryPercentage: 0.6
     }],
     yAxes: [{
-      id: "y-axis-first"
+      // id: "y-axis-first"
     }, {
-      id: "y-axis-second"
+      // id: "y-axis-second"
     }]
   }
 };
